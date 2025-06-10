b9b6640598e197c0d70afb0c7aea89e9f2871f0e
yuanzf <yuanzf@connect.hku.hk>
2025-06-10 09:58:56 +0800

    update test

--- test_ae.sh
+++ test_ae.sh
@@ -7,12 +7,14 @@ id_1=$4
 id_2=$5
 id_3=$6
 id_4=$7
+model_dataset=${MODEL_DATASET:-${dataset}}
 
 id="$id_1 $id_2 $id_3 $id_4"
 
 echo dataset ${dataset}
 echo dev_eval ${dev_eval}
 echo score ${score}
+echo model_dataset ${model_dataset}
 echo id ${id}
 
 tag="id("
@@ -26,6 +28,7 @@ python3 train.py \
     ${dev_eval} \
     -tag=${tag} \
     --use_ids ${id} \
+    --model_dataset=${model_dataset} \
     --score ${score} \
     --test_only 
 
